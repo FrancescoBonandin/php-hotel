f14c0c4326926a1f3eaebf05aec5f776383b4f19
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-08-22 15:57:04 +0200

    EDIT esercizio base fatto

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php
+       $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+        
+    ];
+    $categories = array_keys($hotels[0]);
+    
+    var_dump($categories);
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +52,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>PHP Hotels</title>
 
         <!--Google Fonts-->
         
@@ -39,7 +86,129 @@
         
         <!-- main -->
         <main>
-            
+
+            <table class="table">
+
+
+                    <thead>
+
+                        <tr>
+
+                            <th scope="col">
+                                #
+                            </th>
+
+
+                            <?php
+
+                                foreach($categories as $category ) {
+
+                                
+
+                            ?>
+                                <th scope="col">
+
+                                    <?php
+
+                                        if (str_contains($category,'_')){
+
+                                           $replaced = str_replace ('_', ' ', $category) ;
+
+                                           echo $replaced;
+
+                                        }
+
+                                        else{
+
+                                            echo ucfirst ($category);
+
+                                        }
+
+                                    ?>
+
+                                </th>
+                                
+                            <?php
+
+                                }
+
+                            ?>
+                        </tr>
+
+                    </thead>
+
+                    <tbody>
+
+                        <?php
+        
+                            foreach ($hotels as $index => $hotel) {
+        
+                        ?>
+
+                            <tr>
+
+                       
+
+                                <th scope="row">
+
+                                    <?php
+
+                                        echo $index + 1;
+
+                                    ?>
+
+                                </th>
+
+                                <?php
+
+                                    foreach ($hotel as $key => $value){
+
+
+                                        echo "$key = $value ";
+
+
+                                ?>
+
+                                <td>
+
+                                    
+                                    <?php  
+
+                                        if($key=='parking'&& $value == true){
+                                            echo 'Si';
+                                        }
+
+                                        elseif($key=='parking'&& $value == false){
+                                            echo 'No';
+                                        }
+
+                                        else{
+
+                                            echo "$value";
+
+                                        }
+                                    ?>
+
+                                </td>
+
+                                <?php
+
+                                    }
+
+                                ?>
+
+                            </tr>
+
+
+                        <?php
+
+                            }
+
+                        ?>
+
+                    </tbody>
+            </table>
+
         </main>
         <!-- fine main -->
 
